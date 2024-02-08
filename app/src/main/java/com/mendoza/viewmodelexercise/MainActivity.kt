57c260ce22d7b4468365be7740345c2ae8ef3969
Antonio Mendoza <antonio.mendoza@gmail.com>
2024-02-08 09:49:09 +0100

    Observer

--- app/src/main/java/com/mendoza/viewmodelexercise/MainActivity.kt
+++ app/src/main/java/com/mendoza/viewmodelexercise/MainActivity.kt
@@ -2,17 +2,44 @@ package com.mendoza.viewmodelexercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.mendoza.viewmodelexercise.databinding.ActivityMainBinding
+import com.mendoza.viewmodelexercise.ui.MarcadorViewModel
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var marcadorViewModel: MarcadorViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        marcadorViewModel = ViewModelProvider(this).get(MarcadorViewModel::class.java)
 
+        binding.buttonA.setOnClickListener {
+            marcadorViewModel.haMarcado("A")
+        }
+
+        binding.buttonB.setOnClickListener {
+            marcadorViewModel.haMarcado("B")
+        }
+
+        val tanteoAOberser=Observer<Int>{value ->
+            binding.textViewMarcadorA.text = value.toString()
+            binding.textViewPartido.text = marcadorViewModel.dameResultado()
+        }
+
+        /*marcadorViewModel.tanteoEquipoA.observe(this){value ->
+            binding.textViewMarcadorA.text = value.toString()
+        }*/
+        marcadorViewModel.tanteoEquipoA.observe(this, tanteoAOberser)
+
+        marcadorViewModel.tanteoEquipoB.observe(this){value ->
+            binding.textViewMarcadorB.text = value.toString()
+            binding.textViewPartido.text = marcadorViewModel.dameResultado()
+        }
     }
 }
--- app/src/main/java/com/mendoza/viewmodelexercise/ui/MarcadorViewModel.kt
+++ app/src/main/java/com/mendoza/viewmodelexercise/ui/MarcadorViewModel.kt
@@ -4,11 +4,26 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MarcadorViewModel: ViewModel() {
+
     var tanteoEquipoA = MutableLiveData<Int>()
     var tanteoEquipoB = MutableLiveData<Int>()
 
     init{
         tanteoEquipoA.value = 0
         tanteoEquipoB.value = 0
+    }
+
+    fun haMarcado(equipo: String) {
+        if (equipo.equals("A")){
+            tanteoEquipoA.value = tanteoEquipoA.value!! + 1
+        }
+        else{
+            tanteoEquipoB.value = tanteoEquipoB.value!! + 1
+        }
+
+    }
+
+    fun dameResultado(): String {
+        return tanteoEquipoA.value.toString() + " - " + tanteoEquipoB.value.toString()
     }
 }
